b41dcc2834de5f79af22d794b8e0dc206ac7d1d3
Charats Lo-Badal Burch <burchc@wit.edu>
2022-03-30 14:49:54 -0400

    plz work

--- src/main/java/com/demo/FizzBuzz.java
+++ src/main/java/com/demo/FizzBuzz.java
@@ -1,24 +1,23 @@
 package com.demo;
 
-public class FizzBuzz {
-	public static String processNumber(int num) {	
-		if (num < 0) return "";
-		if (num == 0) return "0";
-		if (num % 15 == 0) return "FizzBuzz";
-		if (num % 3 == 0) return "Fizz";
-		if (num % 5 == 0) return "Buzz";
-		return num+"";
-	}
-	
-	public static String processNumbers (int[] numList) {
-		String proc_num, new_nums = "";
-		for (int i = 0; i < numList.length; i++) {
-			proc_num = processNumber(numList[i]);
-			if ((i == numList.length-1) || (proc_num == "")) {
-				new_nums = new_nums + proc_num;
-		} else {
-				new_nums = new_nums + proc_num + ", ";
-		}
-		return new_nums;
-	}
-}
+public class FizzBuzz { 
+	public static String processNumber(int num) {
+        if (num < 0) return "";
+        if (num == 0) return "0";
+        if (num % 15 == 0) return "FizzBuzz";
+        if (num % 3 == 0) return "Fizz";
+        if (num % 5 == 0) return "Buzz";
+        return num+"";
+    }
+	public static String processNumbers(int[] nums) {
+        String proc_num, new_nums = "";
+        for (int i = 0; i < nums.length; i++) {
+          proc_num = processNumber(nums[i]);
+          if ((i == nums.length-1) || (proc_num == "")) {
+            new_nums = new_nums + proc_num;
+          } else {
+            new_nums = new_nums + proc_num + ", ";
+          }
+        }
+        return new_nums;
+    }
