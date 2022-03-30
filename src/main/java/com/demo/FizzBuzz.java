22e48cb2d2004f48c8f80c5b94ac72000fd57ee5
Charats Lo-Badal Burch <burchc@wit.edu>
2022-03-30 14:39:16 -0400

    Added code to processNumber + processNumbers
    Should be able to pass current tests

--- src/main/java/com/demo/FizzBuzz.java
+++ src/main/java/com/demo/FizzBuzz.java
@@ -4,14 +4,25 @@ public class FizzBuzz
 {
     String processNumber(int num)
 	{	
-    	// Copy your code here
-		return "";
+		if (num < 0) return "";
+		if (num == 0) return "0";
+		if (num % 15 == 0) return "FizzBuzz";
+		if (num % 3 == 0) return "Fizz";
+		if (num % 5 == 0) return "Buzz";
+		return num+"";
 	}
 	
-	String processNumbers (int[] numList)
-	{
-    	// Copy your code here
-		return "";
+	String processNumbers (int[] numList) {
+		String proc_num, new_nums = "";
+		for (int i = 0; i < nums.length; i++) {
+		  proc_num = processNumber(nums[i]);
+		  if ((i == nums.length-1) || (proc_num == "")) {
+		    new_nums = new_nums + proc_num;
+		  } else {
+		    new_nums = new_nums + proc_num + ", ";
+		  }
+		}
+		return new_nums;
 	}
 
 }
