634efdd88c3cf5cee65ec85819536df8250fc047
Charats Lo-Badal Burch <burchc@wit.edu>
2022-03-30 14:42:30 -0400

    Fixed { brackets + spacing

--- src/main/java/com/demo/FizzBuzz.java
+++ src/main/java/com/demo/FizzBuzz.java
@@ -1,9 +1,7 @@
 package com.demo;
 
-public class FizzBuzz 
-{
-    String processNumber(int num)
-	{	
+public class FizzBuzz {
+    String processNumber(int num) {	
 		if (num < 0) return "";
 		if (num == 0) return "0";
 		if (num % 15 == 0) return "FizzBuzz";
@@ -15,14 +13,12 @@ public class FizzBuzz
 	String processNumbers (int[] numList) {
 		String proc_num, new_nums = "";
 		for (int i = 0; i < nums.length; i++) {
-		  proc_num = processNumber(nums[i]);
-		  if ((i == nums.length-1) || (proc_num == "")) {
-		    new_nums = new_nums + proc_num;
-		  } else {
-		    new_nums = new_nums + proc_num + ", ";
-		  }
+			proc_num = processNumber(nums[i]);
+			if ((i == nums.length-1) || (proc_num == "")) {
+				new_nums = new_nums + proc_num;
+		} else {
+			  new_nums = new_nums + proc_num + ", ";
 		}
 		return new_nums;
 	}
-
 }
