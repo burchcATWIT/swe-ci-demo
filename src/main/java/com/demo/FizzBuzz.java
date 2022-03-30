4d943cdd56491187a966a4aa61d31a4c87de9922
Charats Lo-Badal Burch <burchc@wit.edu>
2022-03-30 14:48:13 -0400

    Update FizzBuzz.java

--- src/main/java/com/demo/FizzBuzz.java
+++ src/main/java/com/demo/FizzBuzz.java
@@ -1,7 +1,7 @@
 package com.demo;
 
 public class FizzBuzz {
-    public static String processNumber(int num) {	
+	public static String processNumber(int num) {	
 		if (num < 0) return "";
 		if (num == 0) return "0";
 		if (num % 15 == 0) return "FizzBuzz";
@@ -10,7 +10,7 @@ public class FizzBuzz {
 		return num+"";
 	}
 	
-public static String processNumbers (int[] numList) {
+	public static String processNumbers (int[] numList) {
 		String proc_num, new_nums = "";
 		for (int i = 0; i < numList.length; i++) {
 			proc_num = processNumber(numList[i]);
